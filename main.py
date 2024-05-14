866a779d24a8099d12f1db1b60cddf0709c81f45
Danylo <virus1908@gmail.com>
2024-05-14 22:14:51 +0300

    hw7

--- main.py
+++ main.py
@@ -1,226 +1,98 @@
-import timeit
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
 
-from data import article1, article2
+    def __str__(self, level=0, prefix="Root: "):
+        ret = "\t" * level + prefix + str(self.val) + "\n"
+        if self.left:
+            ret += self.left.__str__(level + 1, "L--- ")
+        if self.right:
+            ret += self.right.__str__(level + 1, "R--- ")
+        return ret
 
 
-class HashTable:
-    def __init__(self, size):
-        self.size = size
-        self.table = [[] for _ in range(self.size)]
-
-    def hash_function(self, key):
-        return hash(key) % self.size
-
-    def insert(self, key, value):
-        key_hash = self.hash_function(key)
-        key_value = [key, value]
-
-        if self.table[key_hash] is None:
-            self.table[key_hash] = list([key_value])
-            return True
+def insert(root, key):
+    if root is None:
+        return Node(key)
+    else:
+        if key < root.val:
+            root.left = insert(root.left, key)
         else:
-            for pair in self.table[key_hash]:
-                if pair[0] == key:
-                    pair[1] = value
-                    return True
-            self.table[key_hash].append(key_value)
-            return True
-
-    def get(self, key):
-        key_hash = self.hash_function(key)
-        if self.table[key_hash] is not None:
-            for pair in self.table[key_hash]:
-                if pair[0] == key:
-                    return pair[1]
-        return None
-
-    def delete(self, key):
-        key_hash = self.hash_function(key)
-        if self.table[key_hash] is not None:
-            pair_to_delete = None
-            for pair in self.table[key_hash]:
-                if pair[0] == key:
-                    pair_to_delete = pair
-            if pair_to_delete is not None:
-                self.table[key_hash].remove(pair_to_delete)
-                return True
-        return False
+            root.right = insert(root.right, key)
+    return root
 
 
-def binary_search(arr, x):
-    assert arr[-1] >= x
-    low = 0
-    high = len(arr) - 1
-    iteration = 1
-
-    while low <= high:
-
-        mid = (high + low) // 2
-
-        if arr[mid] < x:
-            low = mid + 1
-            iteration += 1
-
-        elif arr[mid] > x:
-            high = mid - 1
-            iteration += 1
-
-        else:
-            return iteration, arr[mid]
-
-    return iteration, arr[low]
+def search(root, key):
+    if root is None or root.val == key:
+        return root
+    if key < root.val:
+        return search(root.left, key)
+    return search(root.right, key)
 
 
-def compute_lps(pattern):
-    lps = [0] * len(pattern)
-    length = 0
-    i = 1
-
-    while i < len(pattern):
-        if pattern[i] == pattern[length]:
-            length += 1
-            lps[i] = length
-            i += 1
-        else:
-            if length != 0:
-                length = lps[length - 1]
-            else:
-                lps[i] = 0
-                i += 1
-
-    return lps
+def min_value_node(node) -> Node:
+    current = node
+    while current.left:
+        current = current.left
+    return current
 
 
-# noinspection PyPep8Naming
-def kmp_search(main_string, pattern):
-    M = len(pattern)
-    N = len(main_string)
+def delete(root, key):
+    if not root:
+        return root
 
-    lps = compute_lps(pattern)
-
-    i = j = 0
-
-    while i < N:
-        if pattern[j] == main_string[i]:
-            i += 1
-            j += 1
-        elif j != 0:
-            j = lps[j - 1]
-        else:
-            i += 1
-
-        if j == M:
-            return i - j
-
-    return -1  # якщо підрядок не знайдено
+    if key < root.val:
+        root.left = delete(root.left, key)
+    elif key > root.val:
+        root.right = delete(root.right, key)
+    else:
+        if not root.left:
+            temp = root.right
+            return temp
+        elif not root.right:
+            temp = root.left
+            return temp
+        root.val = min_value_node(root.right).val
+        root.right = delete(root.right, root.val)
+    return root
 
 
-def build_shift_table(pattern):
-    """Створити таблицю зсувів для алгоритму Боєра-Мура."""
-    table = {}
-    length = len(pattern)
-    # Для кожного символу в підрядку встановлюємо зсув рівний довжині підрядка
-    for index, char in enumerate(pattern[:-1]):
-        table[char] = length - index - 1
-    # Якщо символу немає в таблиці, зсув буде дорівнювати довжині підрядка
-    table.setdefault(pattern[-1], length)
-    return table
+def min_value_of_tree(root: Node):
+    return min_value_node(root).val
 
 
-def boyer_moore_search(text, pattern):
-    # Створюємо таблицю зсувів для патерну (підрядка)
-    shift_table = build_shift_table(pattern)
-    i = 0  # Ініціалізуємо початковий індекс для основного тексту
-
-    # Проходимо по основному тексту, порівнюючи з підрядком
-    while i <= len(text) - len(pattern):
-        j = len(pattern) - 1  # Починаємо з кінця підрядка
-
-        # Порівнюємо символи від кінця підрядка до його початку
-        while j >= 0 and text[i + j] == pattern[j]:
-            j -= 1  # Зсуваємось до початку підрядка
-
-        # Якщо весь підрядок збігається, повертаємо його позицію в тексті
-        if j < 0:
-            return i  # Підрядок знайдено
-
-        # Зсуваємо індекс i на основі таблиці зсувів
-        # Це дозволяє "перестрибувати" над неспівпадаючими частинами тексту
-        i += shift_table.get(text[i + len(pattern) - 1], len(pattern))
-
-    # Якщо підрядок не знайдено, повертаємо -1
-    return -1
+def max_value_of_tree(root: Node):
+    current = root
+    while current.right:
+        current = current.right
+    return current.val
 
 
-def polynomial_hash(s, base=256, modulus=101):
-    """
-    Повертає поліноміальний хеш рядка s.
-    """
-    n = len(s)
-    hash_value = 0
-    for i, char in enumerate(s):
-        power_of_base = pow(base, n - i - 1) % modulus
-        hash_value = (hash_value + ord(char) * power_of_base) % modulus
-    return hash_value
-
-
-def rabin_karp_search(main_string, substring):
-    # Довжини основного рядка та підрядка пошуку
-    substring_length = len(substring)
-    main_string_length = len(main_string)
-
-    # Базове число для хешування та модуль
-    base = 256
-    modulus = 101
-
-    # Хеш-значення для підрядка пошуку та поточного відрізка в основному рядку
-    substring_hash = polynomial_hash(substring, base, modulus)
-    current_slice_hash = polynomial_hash(main_string[:substring_length], base, modulus)
-
-    # Попереднє значення для перерахунку хешу
-    h_multiplier = pow(base, substring_length - 1) % modulus
-
-    # Проходимо крізь основний рядок
-    for i in range(main_string_length - substring_length + 1):
-        if substring_hash == current_slice_hash:
-            if main_string[i:i + substring_length] == substring:
-                return i
-
-        if i < main_string_length - substring_length:
-            current_slice_hash = (current_slice_hash - ord(main_string[i]) * h_multiplier) % modulus
-            current_slice_hash = (current_slice_hash * base + ord(main_string[i + substring_length])) % modulus
-            if current_slice_hash < 0:
-                current_slice_hash += modulus
-
-    return -1
-
-
-def do_test(name: str, article: int, substring: str):
-    timeit_report = timeit.timeit(f"{name}(article{article},'{substring}')",
-                                  number=1000,
-                                  setup=f"from __main__ import {name}, article{article}"
-                                  )
-    print('algo:{:<20s} article:{} substring:{} result:{:.4f}'.format(name, article, substring, timeit_report))
-
-
-def compare():
-    do_test("rabin_karp_search", 1, "Стаття")
-    do_test("boyer_moore_search", 1, "Стаття")
-    do_test("kmp_search", 1, "Стаття")
-    do_test("rabin_karp_search", 2, "Стаття")
-    do_test("boyer_moore_search", 2, "Стаття")
-    do_test("kmp_search", 2, "Стаття")
-    do_test("rabin_karp_search", 1, "ЦЬОГО ТЕКСТУ НЕ ІСНУЄ")
-    do_test("boyer_moore_search", 1, "ЦЬОГО ТЕКСТУ НЕ ІСНУЄ")
-    do_test("kmp_search", 1, "ЦЬОГО ТЕКСТУ НЕ ІСНУЄ")
-    do_test("rabin_karp_search", 2, "ЦЬОГО ТЕКСТУ НЕ ІСНУЄ")
-    do_test("boyer_moore_search", 2, "ЦЬОГО ТЕКСТУ НЕ ІСНУЄ")
-    do_test("kmp_search", 2, "ЦЬОГО ТЕКСТУ НЕ ІСНУЄ")
-    pass
+def tree_sum(root: Node):
+    sum_value = root.val
+    if root.left:
+        sum_value += tree_sum(root.left)
+    if root.right:
+        sum_value += tree_sum(root.right)
+    return sum_value
 
 
 def main():
-    compare()
+    root = Node(5)
+    root = insert(root, 3)
+    root = insert(root, 2)
+    root = insert(root, 4)
+    root = insert(root, 7)
+    root = insert(root, 6)
+    root = insert(root, 8)
+
+    root = delete(root, 7)
+    print(root)
+    print(min_value_of_tree(root))
+    print(max_value_of_tree(root))
+    print(tree_sum(root))
 
 
 if __name__ == '__main__':
